02d5106703ee57b6b22f41dd4a22acd1ba8dfe2b
Victor Grigoriu <vgrigoriu@gmail.com>
2014-11-06 10:02:02 +0200

    Add chat hub

---
+++ ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
